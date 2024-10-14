942dcc7a7024beed03c2228ecd7bdf96e09a4ca7
killgregor <killgregor@gmail.com>
2024-10-14 12:33:22 +0200

    Add basic player logic

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
     print("Starting asteroids!")
@@ -12,14 +13,28 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color=(0,0,0))
+
+        for member in updatable:
+            member.update(dt)
+        for member in drawable:
+            member.draw(screen)
+
+        # screen refresh
         pygame.display.flip()
-        clock.tick(60)
-        dt = clock.tick() / 1000
+        #clock.tick(60)
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
